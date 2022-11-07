04a1a16f5bc15c7ee0901e3869d59b0b89563e13
peterayodeji <pb.ayodeji@gmail.com>
2022-11-07 13:56:03 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,6 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+// export const KEY = '23b8e1b2-19b5-4c76-b8b9-aef00ce34017';
+export const KEY = '9b04eec7-bbd5-4c84-931d-33d74cf0d34d'; // Testing key
+export const MODAL_CLOSE_SEC = 1;
---
+++ src/js/views/previewView.js
@@ -0,0 +1,54 @@
+import View from './View.js';
+import icons from 'url:../../img/icons.svg'; // Parcel 2
+
+class PreviewView extends View {
+  _parentElement = '';
+
+  _generateMarkup() {
+    const id = window.location.hash.slice(1);
+
+    return `
+      <li class="preview">
+        <a class="preview__link ${
+          id === this._data.id ? 'preview__link--active' : ''
+        }" href="#${this._data.id}">
+          <figure class="preview__fig">
+            <img src="${this._data.image}" alt="${this._data.title}" />
+          </figure>
+          <div class="preview__data">
+            <h4 class="preview__title">${this._data.title}</h4>
+            <p class="preview__publisher">${this._data.publisher}</p>
+            <div class="preview__user-generated ${
+              this._data.key ? '' : 'hidden'
+            }">
+              <svg>
+                <use href="${icons}#icon-user"></use>
+              </svg>
+            </div>
+          </div>
+        </a>
+      </li>
+    `;
+  }
+
+  // generateMarkup(result) {
+  //   const id = window.location.hash.slice(1);
+  //   return `
+  //     <li class="preview">
+  //       <a class="preview__link ${
+  //         id === result.id ? 'preview__link--active' : ''
+  //       }" href="#${result.id}">
+  //         <figure class="preview__fig">
+  //           <img src="${result.image}" alt="${result.title}" />
+  //         </figure>
+  //         <div class="preview__data">
+  //           <h4 class="preview__title">${result.title}</h4>
+  //           <p class="preview__publisher">${result.publisher}</p>
+  //         </div>
+  //       </a>
+  //     </li>
+  //   `;
+  // }
+}
+
+export default new PreviewView();
